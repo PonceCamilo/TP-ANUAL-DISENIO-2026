fdfad3657334e46f0c2806a26b97a074d3998c13
TatiMartinez4 <tatianainesmartinez@gmail.com>
2026-05-11 23:38:58 -0300

    Atributos de EntidadBeneficiaria

--- donatrack/servicio-donaciones/src/main/java/ar/utn/donatrack/donaciones/model/entidad/EntidadBeneficiaria.java
+++ donatrack/servicio-donaciones/src/main/java/ar/utn/donatrack/donaciones/model/entidad/EntidadBeneficiaria.java
@@ -4,5 +4,12 @@ import java.util.List;
 
 public class EntidadBeneficiaria {
     // TODO: id, razonSocial, direccion, telefono, correosRepresentantes
+    private int id;
+    private String razonSocial;
+    private String direccion; // ver si es string o si hacemos una clase direccion
+    private String telefono;
+    private List<String> correosRepresentantes;
     private List<Necesidad> necesidades;
+    
+
 }
