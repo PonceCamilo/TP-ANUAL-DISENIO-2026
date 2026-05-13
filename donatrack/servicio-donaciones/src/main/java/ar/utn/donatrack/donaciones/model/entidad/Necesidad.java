7650bdbb23b52a953a621b50c090fd77463e8698
luciaarena <larena@frba.utn.edu.ar>
2026-05-13 17:04:37 -0300

    Necidad implementada

--- donatrack/servicio-donaciones/src/main/java/ar/utn/donatrack/donaciones/model/entidad/Necesidad.java
+++ donatrack/servicio-donaciones/src/main/java/ar/utn/donatrack/donaciones/model/entidad/Necesidad.java
@@ -3,9 +3,10 @@ package ar.utn.donatrack.donaciones.model.entidad;
 import ar.utn.donatrack.donaciones.model.categoria.Subcategoria;
 
 public abstract class Necesidad {
-    // TODO: id, subcategoria, descripcion, entidadBeneficiaria
+    private int id;
     protected Subcategoria subcategoria;
     protected String descripcion;
+    private EntidadBeneficiaria entidadBeneficiaria;
 
     public abstract boolean estaSatisfecha();
 }
