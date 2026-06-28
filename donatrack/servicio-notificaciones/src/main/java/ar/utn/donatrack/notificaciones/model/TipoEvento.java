14241c692986a0c136bb1e4d82e70145f6826192
Cynthia Abbate <82001339+Cynthia-21@users.noreply.github.com>
2026-06-28 15:07:28 -0300

    Add new event types to TipoEvento enum
        INICIO_RUTA,
        ENTREGA_EXITOSA,
        ENTREGA_FALLIDA

--- donatrack/servicio-notificaciones/src/main/java/ar/utn/donatrack/notificaciones/model/TipoEvento.java
+++ donatrack/servicio-notificaciones/src/main/java/ar/utn/donatrack/notificaciones/model/TipoEvento.java
@@ -7,11 +7,17 @@ package ar.utn.donatrack.notificaciones.model;
  * - ASIGNACION_DONACION_DONANTE: al donante, cuando su donación fue asignada.
  * - MISION_CUMPLIDA: al donante, cuando completa una misión.
  * - CAMBIO_CATEGORIA: al donante, cuando sube de categoría.
+ * - INICIO_RUTA: a entidades y donantes involucrados cuando el chofer inicia el recorrido.
+ * - ENTREGA_EXITOSA: a la entidad y al donante cuando se confirma la recepción.
+ * - ENTREGA_FALLIDA: a la entidad, al donante y a administradores cuando la entrega no se concretó.
  */
 public enum TipoEvento {
     INACTIVIDAD_DONANTE,
     ASIGNACION_DONACION_ENTIDAD,
     ASIGNACION_DONACION_DONANTE,
     MISION_CUMPLIDA,
-    CAMBIO_CATEGORIA
+    CAMBIO_CATEGORIA,
+    INICIO_RUTA,
+    ENTREGA_EXITOSA,
+    ENTREGA_FALLIDA
 }
